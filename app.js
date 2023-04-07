97825f6cd94b7d19fd642315f7ffd736f8386d51
rajibdas-it <rajibrad@gmail.com>
2023-04-07 12:53:29 +0600

    write schema for products

--- app.js
+++ app.js
@@ -7,6 +7,51 @@ const mongoose = require("mongoose");
 app.use(express.json());
 app.use(cors());
 
+// schema design
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "please provide a name for this product "],
+    trim: true,
+    unique: [true, "Name should be unique"],
+    minLength: [3, "Name must be at least 3 characters"],
+    maxLength: [100, "Name is too large"],
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: [0, "price cant be negative"],
+  },
+  unit: {
+    type: String,
+    required: true,
+    enum: {
+      value: ["Kg", "litre", "pcs"],
+      message: "unit value cant be {VALUE}",
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: [0, "quantity cant be negative"],
+      validate: {
+        validator: (value) => {
+          const isInteger = Number.isInteger(value);
+          if (isInteger) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
+    },
+  },
+});
+
 app.get("/", (req, res) => {
   console.log("Server is running");
 });
